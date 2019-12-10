d6ecbc4ddd595c3017e09562a90ffbc5ba61255d
saba <s.asadolahi@gmail.com>
2019-12-09 21:22:32 -0600

    add missing dependencies

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require('express')
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 const closetmodel= require("./model/closet.model")
-
-//app.use(express.static('public'));
-//Import the mongoose module
 var mongoose = require('mongoose');
+require('dotenv').config()
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb+srv://dbuser:G2eFtnOcBGRY7VE6@cluster0-sy5yh.mongodb.net/continent?retryWrites=true&w=majority'
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+app.use(express.static('public'));
 app.set('view engine', 'pug');
+
+var mongoDB = process.env.MONGO_CONNECT_URI
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req, res) => {
   // const closet = [
